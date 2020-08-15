9fedd87580cc0fd21a3154f1cb60e9f3d2a1ab2c
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-08-15 23:22:13 +0800

    lcenword

--- bubble.c
+++ bubble.c
@@ -1,14 +1,20 @@
-void bubbleSort(int numbers[], int array_size){
+void bubbleSort(int numbers[], int array_size)
+{
   int i, j, temp;
-
-  for (i = (array_size - 1); i >= 0; i--){
-    for (j = 1; j <= i; j++){
-      if (numbers[j-1] > numbers[j]){
-        temp = numbers[j-1];
-        numbers[j-1] = numbers[j];
+  int ctr = 0;
+  for (i = (array_size - 1); i >= 0; i--)
+  {
+    ctr++;
+    for (j = 1; j <= i; j++)
+    {
+      ctr++;
+      if (numbers[j - 1] > numbers[j])
+      {
+        ctr++;
+        temp = numbers[j - 1];
+        numbers[j - 1] = numbers[j];
         numbers[j] = temp;
       }
     }
   }
 }
-
---
+++ generator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void GenerateData(int A[], int N)
+{
+    int i;
+    srand(time(0));
+    for (i = 0; i < N; i++)
+    {
+        A[i] = rand() % N;
+    }
+}
