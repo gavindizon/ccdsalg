802da6b5fb5b3bbd9b7a0061f18f3db26a67c224
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-08-24 09:56:37 +0800

    added heap and insertion to main

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "bubble.c"
 #include "merge.c"
 #include "heap.c"
+#include "insertion.c"
 
 typedef char String[52];
 
@@ -18,8 +19,8 @@ void CopyData(int *A, int *B, int N){
     }
 }
 
-int bSortTime(int *A, int N, int M, double *cpuTime){
-    unsigned long long int ctr; 
+unsigned long long bSortTime(int *A, int N, double *cpuTime){
+    unsigned long long ctr; 
 
     // Start measuring time
     struct timespec begin, end; 
@@ -42,8 +43,8 @@ int bSortTime(int *A, int N, int M, double *cpuTime){
     return ctr;
 }
 
-int mSortTime(int *A, int *temp, int N, int M, double *cpuTime){
-    unsigned long long int ctr; 
+unsigned long long mSortTime(int *A, int *temp, int N, double *cpuTime){
+    unsigned long long ctr; 
     // Start measuring time
     struct timespec begin, end; 
 
@@ -64,16 +65,49 @@ int mSortTime(int *A, int *temp, int N, int M, double *cpuTime){
     return ctr;
 }
 
+unsigned long long iSortTime(int array[], int size, double *elapsedTime){
+	long seconds, nanoseconds;
+	struct timespec begin, end; 
+	unsigned long long insertionCnt = 0;
+		
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+		insertionCnt = insertionSort(array, size);   // <------ Insertion Sort
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);   
+
+	    seconds = end.tv_sec - begin.tv_sec;
+	    nanoseconds = end.tv_nsec - begin.tv_nsec;
+	    *elapsedTime += seconds + nanoseconds*1e-9;
+	
+	return insertionCnt;
+}
+
+unsigned long long hSortTime(int array[], int size, double *elapsedTime){
+	long seconds, nanoseconds;
+	unsigned long long heapCnt = 0;
+	struct timespec begin, end; 
+
+		
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+		heapCnt = heapSort(array, size);   // <------ Insertion Sort
+	    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);   
+
+	    seconds = end.tv_sec - begin.tv_sec;
+	    nanoseconds = end.tv_nsec - begin.tv_nsec;
+	    *elapsedTime += seconds + nanoseconds*1e-9;
+	
+	return heapCnt;
+
+}
+
 int main()
 {
     int *mainData, *tempData, *tempMerge;
     int N = START, M;
     int i;
-    unsigned long long int counters[2]; // change niyo na lang to 6 if pinagsama sama na 
-    double cpuTimes[2] = {0, 0};
-    String filenames[2] = { "results/bubblesort.txt", "results/mergesort.txt" };
+    unsigned long long int counters[4]; // change niyo na lang to 6 if pinagsama sama na 
+    double cpuTimes[4] = {0, 0, 0, 0};
+    String filenames[4] = { "results/bubblesort.txt", "results/mergesort.txt","results/insertionsort.txt","results/heapsort.txt" };
     FILE *fp;
-
 
     mainData = malloc(N * sizeof(int));
     tempData = malloc(N * sizeof(int));
@@ -90,13 +124,21 @@ int main()
 
         for(M = 1; M <= 10; M++){
             CopyData(mainData, tempData, N);
-            counters[0] = bSortTime(tempData, N, M, &cpuTimes[0]);
+            counters[0] = bSortTime(tempData, N, &cpuTimes[0]);
             CopyData(mainData, tempData, N);
-            counters[1] = mSortTime(tempData, tempMerge, N, M, &cpuTimes[1]);
+			counters[1] = mSortTime(tempData, tempMerge, N, &cpuTimes[1]);
+            CopyData(mainData, tempData, N);
+            counters[2] = iSortTime(tempData, N, &cpuTimes[2]);
+            CopyData(mainData, tempData, N);
+			counters[3] = hSortTime(tempData, N, &cpuTimes[3]);
+
         }
         cpuTimes[0] /= 10;
         cpuTimes[1] /= 10;
-        for(i = 0; i < 2; i++){
+        cpuTimes[2] /= 10;
+        cpuTimes[3] /= 10;
+
+        for(i = 0; i < 4; i++){
             if((fp = fopen(filenames[i], "a"))!=NULL){
                 fprintf(fp,"N: %d || counter: %lld || Average MET: %f\n", N, counters[i], cpuTimes[i]);
             }
